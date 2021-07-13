07d113bbc336c3a8ef8f4fd0b3f6fff2e3f856a4
diced <pranaco2@gmail.com>
2021-07-13 10:38:58 -0700

    Add cpu info to get command

--- common/src/main/java/me/diced/serverstats/common/commands/GetCommand.java
+++ common/src/main/java/me/diced/serverstats/common/commands/GetCommand.java
@@ -6,6 +6,9 @@ import me.diced.serverstats.common.command.Context;
 import me.diced.serverstats.common.exporter.Stats;
 import net.kyori.adventure.text.format.NamedTextColor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static me.diced.serverstats.common.plugin.Util.heatmapColor;
 import static net.kyori.adventure.text.Component.*;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
@@ -28,6 +31,7 @@ public class GetCommand extends Command {
                         text().append(text("Stats: ", style(GOLD, BOLD))),
                         text().append(text("TPS: ", GOLD)).append(text(String.format("%.1f", stats.tps), color)),
                         text().append(text("MSPT: ", GOLD)).append(text(String.format("%.1f ms", stats.mspt), color)),
+                        text().append(text("CPU: ", GOLD)).append(text(new BigDecimal(stats.cpu).setScale(2, RoundingMode.HALF_UP) + "%", heatmapColor(stats.cpu, 100.0f))),
                         text().append(text("Players: ", GOLD)).append(text(String.format("%d", stats.playerCount), WHITE)),
                         text().append(text("Free Memory: ", GOLD)).append(text(String.format("%d MB", stats.freeMemory / 1024 / 1024), WHITE)),
                         text().append(text("Total Memory: ", GOLD)).append(text(String.format("%d MB", stats.totalMemory / 1024 / 1024), WHITE)),
