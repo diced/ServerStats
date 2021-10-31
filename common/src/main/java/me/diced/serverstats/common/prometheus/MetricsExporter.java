8587d9e20e34ebb117ce745beea27780e6ef2c45
diced <pranaco2@gmail.com>
2021-10-31 10:22:42 -0700

    Fix #3 (newline at the end of metrics)

--- common/src/main/java/me/diced/serverstats/common/prometheus/MetricsExporter.java
+++ common/src/main/java/me/diced/serverstats/common/prometheus/MetricsExporter.java
@@ -6,13 +6,11 @@ public class MetricsExporter {
     public static String export() {
         StringWriter writer = new StringWriter();
 
-        Metric<?> last = MetricsManager.registeredMetrics.get(MetricsManager.registeredMetrics.size() - 1);
-
         for (Metric<?> metric : MetricsManager.registeredMetrics) {
             if (metric.enabled()) {
                 writer.write("# HELP " + metric.name + '\n');
                 writer.write("# TYPE " + metric.name + " " + metric.type + '\n');
-                writer.write(metric.formatPrometheus() + (metric.name.equals(last.name) ? "" : '\n'));
+                writer.write(metric.formatPrometheus() + '\n');
             }
         }
 
